7a35fd21c95c91ab26a6c65247bee4dcb2112a6d
mott0 <nitonok@gmail.com>
2019-10-20 20:11:57 +0300

    project base

---
+++ NewsClassificationWebService/src/main/java/com/github/mott0/newsclassificationwebservice/NewsClassificationWebServiceApplication.java
@@ -0,0 +1,11 @@
+package com.github.mott0.newsclassificationwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsClassificationWebServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NewsClassificationWebServiceApplication.class, args);
+    }
+}
